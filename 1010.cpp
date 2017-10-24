7fabe224704e83ed094ae2beacb8251dce6b3437
qiaosiyi <qsy2011815@163.com>
2017-10-24 23:46:24 +0800

    aaa

---
+++ 1010.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <deque>
+#include <map>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main(){
+	vector<int> n;
+	int num,i;
+	do{
+		cin >> num;
+		n.push_back(num);
+	}while(cin.get() != '\n');
+
+	if(n[1] != 0){
+		cout << n[0] * n[1] << " " << n[1] - 1;
+	}else{
+		cout << 0;
+	}
+
+	for(i = 2; i < n.size(); i = i + 2){
+		if(n[i + 1] != 0){
+			cout << " " << n[i] * n[i + 1] << " " << n[i + 1] - 1;
+		}else{
+			cout << " " << 0 << " " << 0;
+		}
+	}
+	cout << endl;
+
+	return 0;
+}
